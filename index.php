75d983bedf930b0834d073eac5a5a72eb255c67e
FarshadJahanfard <116713713+FarshadJahanfard@users.noreply.github.com>
2023-10-27 00:00:22 +0100

    Image slider added

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>Home | Modern Fit Gym</title>
   <link rel="stylesheet" href="css/desktop.css" />
+  <script src="../js/img-slider.js"></script>
 </head>
 
 <body>
@@ -16,8 +17,64 @@
 
 
 <div class="main-content">
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores architecto veniam sint accusamus ab mollitia expedita libero? Animi rem repellat ex quos praesentium, officiis quibusdam blanditiis sed ipsum, impedit hic quod tempore dolores reprehenderit officia saepe. Nam expedita, deserunt, incidunt, dicta distinctio dolores accusamus blanditiis ullam voluptatum inventore pariatur quidem est. Maiores distinctio aliquam autem necessitatibus debitis quibusdam reprehenderit nisi accusamus placeat aspernatur in, laborum, suscipit laboriosam illo labore, dicta culpa consequuntur architecto nam facilis quasi! Iusto id eveniet ipsum rerum, error excepturi. Enim quam quibusdam voluptatum ducimus iure corporis voluptate voluptates quos assumenda porro numquam quis omnis temporibus reprehenderit, nobis nihil, iste veritatis nemo officiis fuga ea tenetur eaque? Eligendi earum, sed odit facilis quis modi maiores dolores rerum quod omnis dolore architecto magnam fugiat mollitia placeat sit adipisci error quasi optio soluta eum inventore? Corporis, dignissimos iure corrupti amet necessitatibus assumenda modi cupiditate similique, est dolorum magnam nam sit voluptatibus. Ipsum sit, soluta tenetur atque voluptas consectetur aliquam laudantium accusantium necessitatibus eum sunt neque et provident reiciendis, unde a molestiae perferendis. Unde, ullam non. Cumque quasi eos delectus ea beatae reprehenderit natus dignissimos similique at ut quae explicabo quibusdam itaque quam, quidem ipsum magni, deserunt, voluptas laborum voluptate! Expedita iste eum veritatis, vel praesentium ipsa voluptatem ex placeat quaerat! Illo ipsam totam sit reprehenderit quod necessitatibus ab natus, laudantium id dolores consequuntur animi neque dolorum maxime in, non quis esse. Repudiandae sequi optio impedit repellendus libero itaque soluta quibusdam quia, earum perspiciatis autem natus exercitationem facilis corporis ab eveniet illo consequatur officiis molestias placeat nisi doloremque sapiente minima! Ullam amet sit possimus iste quidem exercitationem cupiditate, nemo in nam officia consectetur veniam reprehenderit facere. Libero et fugit ratione adipisci quis vitae ullam facilis excepturi tempora totam autem blanditiis ex laboriosam, omnis provident in laborum molestias culpa quo voluptas. Itaque nobis asperiores provident, ex quidem quis consequatur voluptatem laboriosam iste cumque repellat distinctio id excepturi aliquam non nihil maxime. Id neque unde pariatur laudantium dolore esse facere distinctio a? Eius atque minus cupiditate error esse. Aliquam et, cumque quidem commodi voluptate debitis alias officia iste! Iure officiis nemo error enim quaerat eaque dicta minus, voluptas impedit, optio laudantium sed ut debitis quasi numquam ipsum velit. Velit tenetur doloremque quia alias fuga ullam, dolores vitae numquam, reprehenderit consequatur repudiandae! Saepe veniam architecto laborum beatae nesciunt est deleniti, eaque, consectetur commodi molestias culpa sed inventore accusantium corporis et optio ipsa nam blanditiis nisi maiores corrupti. Veniam ex quia doloremque doloribus modi accusamus ipsum autem, nulla iste quasi rem consectetur ipsa cumque. Accusantium soluta modi natus fugiat consectetur mollitia, at vel perferendis sunt explicabo neque repellendus minima voluptatibus tempore debitis provident officiis veniam magnam. Modi ea quibusdam dolore reprehenderit! Assumenda fugiat eum est corporis, laudantium voluptatum quod maxime maiores? Itaque laboriosam labore explicabo recusandae, reiciendis aliquid possimus adipisci non ipsa! Quo itaque saepe, illo, quis necessitatibus perferendis reprehenderit quos totam fuga incidunt repellat ullam omnis quasi quidem molestiae ea modi amet, ab nobis eos sit cum voluptate adipisci. Iure fugiat rem velit error harum obcaecati animi in! Itaque consectetur atque minima modi veniam, nemo quod iure fugiat aut perferendis nesciunt nisi quisquam! Aliquam qui quam officiis, possimus id quos consectetur consequuntur est ea aperiam! Tempore omnis illum aliquid dolores deserunt voluptate debitis sed, magni laudantium nostrum quis quia! Ullam praesentium ea cumque inventore illum quibusdam, ab, error quam aperiam, doloribus commodi repudiandae ipsa. Illum rerum aliquid aspernatur dolor sapiente et consectetur unde quia enim architecto. Fuga beatae, incidunt harum nobis quos dolorem labore asperiores magni facilis possimus earum suscipit alias ad inventore nisi voluptatum soluta quaerat nostrum enim hic aliquam, voluptates tempore.
+
+
+<!-- <div class="slider-container">
+    <div class="slider">
+        <img src="./Images/gym1.jpg" alt="Image 1">
+        <img src="i./Images/gym1.jpg" alt="Image 2">
+        <img src="./Images/gym1.jpg" alt="Image 3">
+        <img src="./Images/gym1.jpg" alt="Image 4">
+        <img src="./Images/gym1.jpg" alt="Image 5">
+        <img src="./Images/gym1.jpg" alt="Image 6">
+    </div>
+    <button class="prev" onclick="prevSlide()">Previous</button>
+    <button class="next" onclick="nextSlide()">Next</button>
+</div> -->
+
+
+
+<div class="slideshow-container">
+
+
+
+<div class="slideshow-container">
+
+<div class="mySlides fade">
+
+  <img src="./Images/gym2.jpg" style="width:100%">
+
 </div>
+<div class="mySlides fade">
+  <img src="./Images/gym3.jpg" style="width:100%">
+  
+</div>
+
+<div class="mySlides fade">
+  <img src="./Images/gym2.jpg" style="width:100%">
+</div>
+
+<a class="prev" onclick="plusSlides(-1)">❮</a>
+<a class="next" onclick="plusSlides(1)">❯</a>
+
+</div>
+<br>
+
+<div style="text-align:center">
+  <span class="dot" onclick="currentSlide(1)"></span> 
+  <span class="dot" onclick="currentSlide(2)"></span> 
+  <span class="dot" onclick="currentSlide(3)"></span> 
+</div>
+
+
+
+
+
+</div>
+
+
+
+
 
 
 
