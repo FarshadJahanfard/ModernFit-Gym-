30245424812747d877ea054e1baca20b8ad129e6
Anton <antonio.fiasco24@gmail.com>
2023-12-29 16:12:37 +0000

    feat: nutrition progress page date filter

--- resources/views/assignments/diet/progress.blade.php
+++ resources/views/assignments/diet/progress.blade.php
@@ -18,7 +18,12 @@
                 <h3>Diet Plan Nutritional Information</h3>
 
                 @php
-                    $todayLogs = $logs->filter(function ($log) {
+                    // Adjust this logic to use the filtered logs based on the selected date
+                    $logsForNutrition = $logs->filter(function ($log) {
+                        return $log->pivot->created_at->format('Y-m-d') == request('filterDate');
+                    });
+
+                    $todayLogs = $logsForNutrition->filter(function ($log) {
                         return $log->pivot->created_at->isToday();
                     });
                 @endphp
@@ -48,7 +53,16 @@
 
         <div class="row mt-4">
             <div class="col-md-12">
-                <h3>Diet Logs</h3>
+                <div class="d-flex justify-content-between">
+                    <h3 class="align-self-end">Diet Logs</h3>
+                    <form class="d-flex gap-2" action="{{ url()->current() }}" method="GET">
+                        <div class="form-group mr-2">
+                            <label for="filterDate">Filter by Date:</label>
+                            <input type="date" id="filterDate" name="filterDate" class="form-control" value="{{ request('filterDate') }}">
+                        </div>
+                        <button type="submit" class="align-self-center btn btn-primary mt-3">Apply Filter</button>
+                    </form>
+                </div>
                 <table class="table table-bordered table-striped">
                     <thead>
                     <tr>
@@ -70,6 +84,10 @@
                     @endphp
 
                     @foreach($sortedDates as $date)
+                        @if(request('filterDate') && $date != request('filterDate'))
+                            @continue
+                        @endif
+
                         <tr class="table-primary">
                             <td colspan="7" class="font-weight-bold">{{ $date }}</td>
                         </tr>
